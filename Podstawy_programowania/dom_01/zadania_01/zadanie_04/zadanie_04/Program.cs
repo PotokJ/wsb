ac82a806cb350a61d9438173ddf5fffbba17f244
PotokJ <jakubpotok4@gmail.com>
2019-10-05 10:26:31 +0200

    dom

---
+++ Podstawy_programowania/dom_01/zadania_01/zadanie_04/zadanie_04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Program liczy sumę cyfr rozwinięcia dziesiętnego podanej liczby naturalnej");
+            Console.Write("Podaj liczbę naturalną: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = n; i > 0; i = i / 10)
+            {
+                sum = sum + i % 10;
+            }
+
+            Console.WriteLine("Suma cyfr tej liczby wynosi: " + sum);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Podstawy_programowania/dom_01/zadania_01/zadanie_05/zadanie_05/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Program sprawdza czy podana liczba jest liczbą pierwszą");
+
+            for (; ; )
+            {
+
+                Console.Write("Podaj liczbę: ");
+                int n = int.Parse(Console.ReadLine());
+                if (IsPrime(n))
+                {
+                    Console.WriteLine("{0} jest liczbą pierwszą", n);
+                }
+                else
+                {
+                    Console.WriteLine("{0} nie jest liczbą pierwszą", n);
+                }
+            }
+
+            bool IsPrime(int n)
+            {
+                if (n > 1)
+                {
+                    return Enumerable.Range(1, n).Where(x => n % x == 0)
+                                     .SequenceEqual(new[] { 1, n });
+                }
+
+                return false;
+            }
+
+        }
+    }
+}
